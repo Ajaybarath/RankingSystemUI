272fb91df44d90a701528a4cb66411af31b3d1ae
Ajaybarath3 <ajaybarath@transvision.us>
2024-03-09 00:33:48 +0530

    added pagination and changed title

--- src/CanditatesList.js
+++ src/CanditatesList.js
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 
 const CanditateList = () => {
     const [data, setData] = useState([]);
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
     const [total, setTotal] = useState(0);
 
     const navigate = useNavigate();
@@ -17,11 +17,13 @@ const CanditateList = () => {
 
     const fetchData = async () => {
         try {
-            const url = `${apiUrl}/api/v1/candidate?page=${page}`;
+            const url = `${apiUrl}/api/v1/candidate?page=${(page - 1)}&&size=10`;
             const response = await axios.get(url);
             console.log(response.data.result.data);
             setData(response.data.result.data);
             setTotal(response.data.result.totalPages);
+            console.log(pagination.current);
+
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -67,7 +69,7 @@ const CanditateList = () => {
     const pagination = {
         total: total,
         pageSize: 10,
-        current: (page + 1),
+        current: page,
         onChange: (page) => setPage(page),
     };
 
@@ -82,7 +84,15 @@ const CanditateList = () => {
 
     return (
         <div>
-            <Table className='antTable' columns={columns} dataSource={data} pagination={pagination} onRow={(record) => ({ onClick: () => handleRowClick(record) })} rowKey='rollNumber' />
+            <Table className='antTable'
+                columns={columns} dataSource={data}
+                pagination={pagination}
+                onRow={(record) => ({ onClick: () => handleRowClick(record) })}
+                onChange={(pagination) => {
+                    console.log(pagination.current);
+                    return setPage(pagination.current)
+                }}
+                rowKey='rollNumber' />
         </div>
     );
 };
